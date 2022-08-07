707fa5d05a76048ec0a2c2c684c38e060622ee20
Vika Dovgal <vikadovgal@yandex.ru>
2022-08-07 19:57:33 +0400

    rateLimit fix

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 100, // можно совершить максимум 100 запросов с одного IP
+  max: 10000, // можно совершить максимум 10000 запросов с одного IP
 });
 
 module.exports = {
